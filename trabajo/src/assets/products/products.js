9752c82d39c863b746d6cd9d337dc035995f2e76
nicomarset <nicolasmarsetg@gmail.com>
2024-05-15 23:55:36 +0200

    primera entrega

---
+++ trabajo/src/assets/products/products.js
@@ -0,0 +1,55 @@
+import hd1 from '../images/hd1.jpg'
+import hd2 from '../images/hd2.jpg'
+
+export default [
+    {'img': hd1,
+    'title': 'remera 1',
+    'desc': '...',
+    'price': '$999'
+    },
+    {'img': hd2,
+    'title': "remera 2",
+    'desc': '...',
+    'price': '$5999'
+    },
+    {'img': hd1,
+    'title': 'remera 3',
+    'desc': '...',
+    'price': '$2999'
+    },
+    {'img': hd2,
+    'title': "remera 4",
+    'desc': '...',
+    'price': '$1999'
+    },
+    {'img': hd1,
+    'title': 'remera 5',
+    'desc': '...',
+    'price': '$2600'
+    },
+    {'img': hd2,
+    'title': "remera 6",
+    'desc': '...',
+    'price': '$2399'
+    },
+    {'img': hd1,
+    'title': 'remera 7',
+    'desc': '...',
+    'price': '$1789'
+    },
+    {'img': hd2,
+    'title': "remera 8",
+    'desc': '...',
+    'price': '$3579'
+    },
+    {'img': hd1,
+    'title': "remera 9",
+    'desc': '...',
+    'price': '$2220'
+    },
+    {'img': hd2,
+    'title': "remera 10",
+    'desc': '...',
+    'price': '$1860'
+    }
+]
